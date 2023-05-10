f698c1c194bd2296c7736687d28105a40db2840a
alexanderinglot <132993691+alexanderinglot@users.noreply.github.com>
2023-05-10 19:39:19 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,3 @@
-import notification.InternetNews;
-import notification.RadioNews;
-import notification.TvNews;
-import weather_forecast.WeatherForecast;
-
 public class Main {
     public static void main(String[] args) {
         
