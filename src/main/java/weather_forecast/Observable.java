903f239fa7fbed3e8231df03831a54c509975c2b
alexanderinglot <132993691+alexanderinglot@users.noreply.github.com>
2023-05-10 19:39:50 +0200

    Delete src/main/java/weather_forecast directory

--- src/main/java/weather_forecast/Observable.java
+++
@@ -1,9 +0,0 @@
-package weather_forecast;
-
-import notification.Observer;
-
-public interface Observable {
-    void registerObserver(Observer observer);
-    void unregisterObserver(Observer observer);
-    void notifyObservers();
-}
