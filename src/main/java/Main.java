58f722b573c3a1c31c94d3ff39b237c652cab8e2
aleksanderinglot <57831465+aleksanderinglot@users.noreply.github.com>
2023-05-11 09:11:28 +0200

    builder inner class implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        FlightLeg leg = new FlightLeg.FlightLegBuilder("Las Vegas", "Los Angeles").price(50).build();
 
+        System.out.println(leg);
     }
 }
